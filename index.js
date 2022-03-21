b78bd81c358fb3f8389b1c7780f97b8d8196c9d4
des-ant <57375593+des-ant@users.noreply.github.com>
2022-03-21 21:58:14 +1100

    Add console printing to single notes and all notes paths for debugging

--- index.js
+++ index.js
@@ -41,6 +41,9 @@ Note.sync()
 
 app.get('/api/notes', async (req, res) => {
   const notes = await Note.findAll()
+
+  console.log(JSON.stringify(notes, null, 2))
+
   res.json(notes)
 })
 
@@ -56,6 +59,7 @@ app.post('/api/notes', async (req, res) => {
 app.get('/api/notes/:id', async (req, res) => {
   const note = await Note.findByPk(req.params.id)
   if (note) {
+    console.log(note.toJSON())
     res.json(note)
   } else {
     res.status(404).end()
