f94849cbe69d04a0b3db0efdfd5bda2475cc61b9
des-ant <57375593+des-ant@users.noreply.github.com>
2022-03-26 11:44:02 +1100

    Change the note insertion by making it work the same as in part 4, i.e. the creation of a note can only be successful if the request corresponding to the creation is accompanied by a valid token from login. The note is then stored in the list of notes created by the user identified by the token.

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,11 @@ const router = require('express').Router()
 const { User } = require('../models')
 
 router.get('/', async (req, res) => {
-  const users = await User.findAll()
+  const users = await User.findAll({
+    include: {
+      model: Note
+    }
+  })
   res.json(users)
 })
 
