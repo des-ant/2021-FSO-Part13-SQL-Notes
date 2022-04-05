e79410fec1b18fa4efb779237cf9958ebc245afe
des-ant <57375593+des-ant@users.noreply.github.com>
2022-04-05 21:00:20 +1000

    Expand the application so that each user can be added to one or more teams.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,14 @@
 const Note = require('./note')
 const User = require('./user')
+const Team = require('./team')
+const Membership = require('./membership')
 
 Note.belongsTo(User)
 User.hasMany(Note)
 
+User.belongsToMany(Team, { through: Membership })
+Team.belongsToMany(User, { through: Membership })
+
 module.exports = {
-  Note, User
+  Note, User, Team, Membership
 }
