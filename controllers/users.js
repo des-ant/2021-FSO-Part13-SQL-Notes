40e873ff5a1e6fe6034071c73ef662421b7c8875
des-ant <57375593+des-ant@users.noreply.github.com>
2022-04-06 22:43:24 +1000

    Update application so that the note can be associated with other users, and that a user can be associated with an arbitrary number of notes created by other users.

--- controllers/users.js
+++ controllers/users.js
@@ -57,19 +57,35 @@ router.post('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const user = await User.findByPk(req.params.id, {
-    include: {
-        model: Note
-      }
-    }
-  )
+    attributes: { exclude: [''] } ,
+    include:[{
+        model: Note,
+        attributes: { exclude: ['userId'] }
+      },
+      {
+        model: Note,
+        as: 'marked_notes',
+        attributes: { exclude: ['userId']},
+        through: {
+          attributes: []
+        },
+        include: {
+          model: User,
+          attributes: ['name']
+        }
+      },
+      {
+        model: Team,
+        attributes: ['name', 'id'],
+        through: {
+          attributes: []
+        }
+      },
+    ]
+  })
 
   if (user) {
-    res.json({
-      username: user.username,
-      name: user.name,
-      note_count: user.notes.length
-    })
-
+    res.json(user)
   } else {
     res.status(404).end()
   }
--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Note = require('./note')
 const User = require('./user')
 const Team = require('./team')
 const Membership = require('./membership')
+const UserNotes = require('./user_notes')
 
 Note.belongsTo(User)
 User.hasMany(Note)
@@ -9,6 +10,9 @@ User.hasMany(Note)
 User.belongsToMany(Team, { through: Membership })
 Team.belongsToMany(User, { through: Membership })
 
+User.belongsToMany(Note, { through: UserNotes, as: 'marked_notes' })
+Note.belongsToMany(User, { through: UserNotes, as: 'users_marked' })
+
 module.exports = {
-  Note, User, Team, Membership
+  Note, User, Team, Membership, UserNotes
 }
